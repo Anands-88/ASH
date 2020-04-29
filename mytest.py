d03829582d58e217d1ec7e4e4764099149982497
Anand S <aravindas4>
2020-04-29 18:39:06 +0530

    Add new files

---
+++ mytest.py
@@ -0,0 +1,7 @@
+for a in range(0,201): 
+    if a<10:
+        print(str(a)+(' --------> ') + chr(a))
+    elif a<100:
+        print(str(a)+(' -------> ') + chr(a))
+    elif a<200:
+        print(str(a)+(' ------> ') + chr(a))
